47d0f57a462d9e2f09af8c153d355ae530189532
IEPiXI <0epix0@gmail.com>
2023-03-10 03:28:22 +0100

    Updated Assistant

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from chatgpt import Assistant
 
 
 def main():
-    assistant = Assistant()
+    assistant = Assistant(  system_message = "You are a helpful assistant.", 
+                            user_message = "Who won the world series in 2020?", 
+                            assistant_message = "The Los Angeles Dodgers won the World Series in 2020."
+                        )
+                        
     assistant.get_api_key()
     answer = assistant.send_message("Hey how are you?")
     print(answer)
-    
+
 if __name__ == "__main__":
     main()
 
