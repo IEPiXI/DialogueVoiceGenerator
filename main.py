44138004692fddcbd15f5853c0e7336c59a7321a
IEPiXI <0epix0@gmail.com>
2023-03-10 03:20:29 +0100

    Implemented small API for chatGPT
    - Implemented Assistant Class using the Chat GPT API
    -.env var should include API KEY

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+from chatgpt import Assistant
+
+
+def main():
+    assistant = Assistant()
+    assistant.get_api_key()
+    answer = assistant.send_message("Hey how are you?")
+    print(answer)
+    
+if __name__ == "__main__":
+    main()
+
