3b13b487ffc0d4cac5bff818e0297ee0a09764a8
nfreitag <nfreitag@uni-koblenz.de>
2023-03-12 02:29:34 +0100

    generate_audios_from_text implemented

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from time import sleep
 from chatgpt import Assistant
 from elevenlabs import ElevenLabHelper
 
@@ -7,38 +8,43 @@ PATH_SYSTEM_MESSAGE = "example/system_message.txt"
 PATH_USER_MESSAGE = "example/user_message.txt"
 PATH_ASSISTANT_MESSAGE = "example/assistant_message.txt"
 
-PATH_OUTPUTS = "outputs/"
+PATH_OUTPUTS = "outputs/out.txt"
+class Main():
 
-def main():
-     
-    system_message = load_message(PATH_SYSTEM_MESSAGE)
-    user_message = load_message(PATH_USER_MESSAGE)
-    assistant_message = load_message(PATH_ASSISTANT_MESSAGE)
 
-    assistant = Assistant(system_message = system_message, user_message = user_message, assistant_message = assistant_message)
-    assistant.get_api_key()
+    def __init__(self):
 
-    answer = assistant.send_message("""Provide a funny conversation between president Biden, former president Trump and former president Obama. 
-                                              Let them pretend to play Rocket League while they flame each other using profaine language. Leave out any information 
-                                              that doesn't relate to a person talking. Also leave out descriptions of expressing feelings.""")
+        self.elevenlab = ElevenLabHelper()
 
-    with open(PATH_OUTPUTS+"out.txt", "w") as text_file:
-       text_file.write(answer)
-    
-    generate_audios_from_text(answer)
+        self.system_message = self.load_message(PATH_SYSTEM_MESSAGE)
+        self.user_message = self.load_message(PATH_USER_MESSAGE)
+        self.assistant_message = self.load_message(PATH_ASSISTANT_MESSAGE)
 
-def load_message(path):
-    with open(path, "r") as text_file:
-       return "".join(text_file.readlines())
+        self.assistant = Assistant(system_message = self.system_message, user_message = self.user_message, assistant_message = self.assistant_message)
+        self.assistant.get_api_key()
 
-def generate_audios_from_text(text):
-    pass
+        #answer = assistant.send_message("""Provide a funny conversation between president Biden, former president Trump and former president Obama. 
+        #                                          Let them pretend to play Rocket League while they flame each other using profaine language. Leave out any information 
+        #                                          that doesn't relate to a person talking. Also leave out descriptions of expressing feelings.""")
 
-def main2():
-    elevenlab = ElevenLabHelper()
-    elevenlab.generate_audio(PATH_TO_AUDIOS,"1","your mum is gay","Rachel")
+        #with open(PATH_OUTPUTS+"out.txt", "w") as text_file:
+        #   text_file.write(answer)
+
+    def load_message(self, path):
+        with open(path, "r") as text_file:
+            return "".join(text_file.readlines())
+
+    def generate_audios_from_text(self):
+        count = 0
+        dialogue_arr = self.load_message(PATH_OUTPUTS).split("\n")
+        for item in dialogue_arr:
+            if not item == '':
+                name, sentence = item.split(":")
+                self.elevenlab.generate_audio(PATH_AUDIOS, str(count) + "_" + name, sentence, name)
+                sleep(1)
+                count += 1 
 
 if __name__ == "__main__":
-    main()
-
+    main = Main()
+    main.generate_audios_from_text()
 
